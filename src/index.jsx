70085ef28a11a496c53350d2fdcf26965e4dd097
zhengpengfei <272546896@qq.com>
2018-03-05 15:14:26 +0800

    v0.0.3

--- src/index.jsx
+++ src/index.jsx
@@ -8,12 +8,26 @@ class Clock extends React.Component {
     this.state = { date: new Date() };
   }
 
+  // getDefaultProps(){
+  //   console.log('getDefaultProps');
+  // }
+
+  getInitialState(){
+    console.log('getInitialState');
+  }
+
+  componentWillMount(){
+    console.log('componentWillMount');
+  }
+
+
   tick() {
     this.setState({ date: new Date() });
   }
 
   //当组件挂载完成时
   componentDidMount() {
+    console.log('componentDidMount');
     this.timer = setInterval(() => {
       this.tick();
     }, 1000);
@@ -21,10 +35,12 @@ class Clock extends React.Component {
 
   //当组件要释放内存空间时
   componentWillUnmount() {
+    console.log('componentDidMount');
     clearInterval(this.timer);
   }
 
   render() {
+    console.log('render');
     return (
       <h1>当时时间：{this.state.date.toLocaleTimeString()}</h1>
     )
